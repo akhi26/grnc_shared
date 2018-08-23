682ced8db667ddcdcae65f94fabfb936473bc3d7
testuser <testuser@example.com>
2018-08-23 19:36:06 +0530

    Replaced ex_statsd with statix

--- mix.exs
+++ mix.exs
@@ -35,7 +35,7 @@ defmodule GrncShared.Mixfile do
       {:ex_doc, "~> 0.14", only: :dev},
       {:earmark, "~> 1.0", only: :dev},
       {:credo, "~> 0.4", only: [:dev, :test]},
-      {:ex_statsd, github: "akhi26/ex_statsd"},
+      {:statix, github: "lexmag/statix"},
     ]
   end
 end
